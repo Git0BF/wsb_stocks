91480028a67997c4b582a1a0908fbcd60e426493
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-04 18:02:00 +0200

    Add files via upload

---
+++ wsb_financial_dashboard.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import yfinance
+import praw
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import pandas as pd
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objs as go
+
+
+reddit = praw.Reddit()
+
+#List of text
+wordsc=[]
+substop= reddit.subreddit('wallstreetbets').hot(limit=250)
+for sub in substop:
+    text=sub.selftext
+    text_wds=text.split()
+    wordsc.append(text_wds)
+
+wordsct=[]
+substop= reddit.subreddit('wallstreetbets').hot(limit=250)
+for sub in substop:
+    title=sub.title
+    title_wds=title.split()
+    wordsct.append(title_wds)
+    
+wordsc=wordsct+wordsc
+
+#Build a list of words from the array.
+newwordsc = []
+for i in wordsc:
+    for j in i:
+        newwordsc.append(j)
+
+#Extract tickers
+stock_symbol=[]
+for word in newwordsc:
+    if word[0]=='$':
+        stock_symbol.append(word)
+        
+cloud1=[]
+for i in newwordsc:
+    i=''.join(filter(str.isalpha, i))
+    cloud1.append(i)
+
+while("" in cloud1) :
+    cloud1.remove("")
+    
+#Build the word cloud.
+wordcloud = WordCloud(width = 1000, height = 500).generate(" ".join(cloud1))
+fig=plt.figure(figsize=(15,8))
+fig=plt.imshow(wordcloud)
+fig=plt.axis("off")
+fig=plt.show()
+st.pyplot(fig)
+plt.close()       
