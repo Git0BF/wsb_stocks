4f67395e7017d8f77e4a27c80ab793b979ed8c4f
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-04 20:10:05 +0200

    Update wsb_financial_dashboard.py

--- wsb_financial_dashboard.py
+++ wsb_financial_dashboard.py
@@ -112,17 +112,11 @@ st.plotly_chart(fig)
 
 #Display revenue and earnings
 
-col1, col2 = st.columns(2)
-
 fin = yf.Ticker("snap")
 df1 = pd.DataFrame(fin.quarterly_earnings)
 
 fige = px.bar(df1, x=df1.index, y=df1.columns)
 
-with col1:
-    st.dataframe(df1)
-with col2:
-    st.plotly_chart(fige)
-
+st.plotly_chart(fige)
 
 st.stop()
