678d1746ac7e2ecb81ee6682a6759b59381b6bad
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-07 20:04:15 +0200

    Update wsb_financial_dashboard.py

--- wsb_financial_dashboard.py
+++ wsb_financial_dashboard.py
@@ -73,7 +73,7 @@ for i in stockfinal:
     stocks.append(i)
 
 #Remove false tickers   
-trolls=['AMTD','CUM','CGX','APE']
+trolls=['AMTD','CUM','CGX','APE','ASS','GMEAMCBBBY']
 for troll in trolls:
     while troll in stocks: 
         stocks.remove(troll)
